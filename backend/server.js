7ad9fe393006f3fa2271f8e99b09a037d9f2ad7c
Sai Rajath <sairajath7@gmail.com>
2024-03-08 20:14:08 -0500

    Signup functionality

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,11 @@ import authRoutes from "./routes/auth.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 dotenv.config();
 
-const PORT = process.env.PORT || 5000;
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 

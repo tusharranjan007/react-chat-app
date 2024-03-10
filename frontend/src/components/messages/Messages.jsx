e0765f645b1d5c1ff7fabc75fc1af7bb24404bf6
Sai Rajath <sairajath7@gmail.com>
2024-03-10 11:25:58 -0400

    Search conversation

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import useGetMessages from "../../hooks/useGetMessages";
 import MessageSkeleton from "../skeletons/MessageSkeleton";
 import Message from "./Message";

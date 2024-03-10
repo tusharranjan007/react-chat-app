c694d13fa5fd82fdc2e56f7580c0713b22790b6a
Sai Rajath <48343927+TheRajath@users.noreply.github.com>
2024-03-10 22:30:55 +0530

    Add socket.io for real time messages (#1)

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,5 +1,6 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
 export const getMessages = async (req, res) => {
 
@@ -57,6 +58,13 @@ export const sendMessage = async (req, res) => {
     await Promise.all([conversation.save(), newMessage.save()]);
 
     // Socket IO functionality will go here
+    const receiverSocketId = getReceiverSocketId(receiverId);
+
+    if (receiverSocketId) {
+      // io.to(<socket_id>).emit() used to send events to specific client
+      io.to(receiverSocketId).emit("newMessage", newMessage);
+    }
+
 
     res.status(201).json(newMessage);
 
--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -3,15 +3,16 @@ import Messages from "./Messages";
 import { TiMessages } from "react-icons/ti";
 import useConversation from "../../store/useConversation";
 import { useEffect } from "react";
+import { useAuthContext } from "../../context/AuthContext";
 
 const MessageContainer = () => {
 
   const { selectedConversation, setSelectedConversation } = useConversation();
 
   useEffect(() => {
-		// cleanup function (unmounts)
-		return () => setSelectedConversation(null);
-	}, [setSelectedConversation]);
+    // cleanup function (unmounts)
+    return () => setSelectedConversation(null);
+  }, [setSelectedConversation]);
 
   return (
     <div className='md:min-w-[450px] flex flex-col'>
@@ -35,10 +36,11 @@ const MessageContainer = () => {
 export default MessageContainer;
 
 const NoChatSelected = () => {
+  const { authUser } = useAuthContext();
   return (
     <div className='flex items-center justify-center w-full h-full'>
       <div className='px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2'>
-        <p>Welcome 👋 John Doe ❄</p>
+        <p>Welcome 👋 {authUser.fullName} ❄</p>
         <p>Select a chat to start messaging</p>
         <TiMessages className='text-3xl md:text-6xl text-center' />
       </div>
